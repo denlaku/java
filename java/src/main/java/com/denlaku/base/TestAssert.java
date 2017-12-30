d7cdc9809f3e45b6e084d0946f169a0d1d157ca4
denlaku <it.tl@qq.com>
2017-12-30 11:22:23 +0800

    add

---
+++ java/src/main/java/com/denlaku/base/TestAssert.java
@@ -0,0 +1,15 @@
+package com.denlaku.base;
+
+import org.junit.Test;
+
+public class TestAssert {
+
+	@Test
+	public void test01() {
+		run(110);
+	}
+	
+	public void run(int i) {
+		assert i > 10 : "i---";
+	}
+}
