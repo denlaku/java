a4fe7bcbb08aaa79e067676afa656b101157c478
denlaku <it.tl@qq.com>
2017-12-30 13:03:43 +0800

    add

--- java/src/main/jdk/com/denlaku/jdk8/newapi/StringJoinerTest.java
+++ java/src/main/jdk/com/denlaku/jdk8/newapi/StringJoinerTest.java
@@ -1,12 +1,11 @@
-package com.denlaku.jdk8.string;
+package com.denlaku.jdk8.newapi;
 
 import java.util.StringJoiner;
 import java.util.StringTokenizer;
 
 import org.junit.Test;
 
-public class TestClass {
-
+public class StringJoinerTest {
 	@Test
 	public void test02() {
 		StringJoiner sj = new StringJoiner("|");
@@ -15,10 +14,20 @@ public class TestClass {
 		sj.add("F");
 		System.out.println(sj.toString());
 		System.out.println(sj.length());
+		
+		StringJoiner sj2 = new StringJoiner("*");
+		sj2.add("1");
+		sj2.add("2");
+		
+		sj.merge(sj2);
+		System.out.println(sj.toString());
+		System.out.println(sj.length());
+		
 	}
-	
+
 	@Test
 	public void test01() {
+		// 字符串拆分
 		StringTokenizer st = new StringTokenizer("a,b.g|p", ".,|");
 		while (st.hasMoreElements()) {
 			System.out.println(st.nextElement());
