661f604e3d2a0beaa66c517496a3178f70be3124
denlaku <it.tl@qq.com>
2018-01-02 18:12:46 +0800

    update

--- java/src/main/java/com/denlaku/socket/client/RpcClient.java
+++ java/src/main/java/com/denlaku/socket/client/RpcClient.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Proxy;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public class RpcClient<T> {
+public class RpcClient {
 	@SuppressWarnings("unchecked")
 	public static <T> T getRemoteProxyObj(final Class<?> serviceInterface, final InetSocketAddress addr) {
 		// 1.将本地的接口调用转换成JDK的动态代理，在动态代理中实现接口的远程调用
